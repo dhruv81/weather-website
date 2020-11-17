45460f3755b2e096bb2fa53a265e509d7c57f1e6
unknown <dhruv.hirani81@gmail.com>
2020-11-17 10:16:20 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-//const { response } = require("express")
-
-console.log('client side java script file loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
